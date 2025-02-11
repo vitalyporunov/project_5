d0132563b83b0dd3e35d8021cf4dd77b238c04d8
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2025-02-11 19:05:56 +0000

    Password recovery

--- config/settings.py
+++ config/settings.py
@@ -143,8 +143,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vitaly.porunov@gmail.com'      
-EMAIL_HOST_PASSWORD = 'Smcjketd03!'    
+EMAIL_HOST_USER = 'porunovsasha@gmail.com'      
+EMAIL_HOST_PASSWORD = '06Sasha1973'
+DEFAULT_FROM_EMAIL = 'porunovsasha@gmail.com>'    
 
 LOGIN_REDIRECT_URL = '/projects/' 
 LOGOUT_REDIRECT_URL = '/'
--- messaging/urls.py
+++ messaging/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views  
 
 urlpatterns = [
-    path('', views.inbox, name='inbox'),  # ✅ Inbox view
-    path('send/', views.send_message, name='send_message'),  # ✅ Send message view
-    path('<int:pk>/', views.message_detail, name='message_detail'),  # ✅ Message detail view
-    path('archived/', views.archived_messages, name='archived_messages'),  # ✅ Archive message view
+    path('', views.inbox, name='inbox'),  
+    path('send/', views.send_message, name='send_message'),  
+    path('<int:pk>/', views.message_detail, name='message_detail'),  
+    path('archived/', views.archived_messages, name='archived_messages'), 
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'), 
 ]
