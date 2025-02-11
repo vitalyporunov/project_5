c715c2173ac951bbd183f29b94b4ea1073fd69e8
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2025-02-11 21:48:10 +0000

    Remove exposed SendGrid API key

--- config/settings.py
+++ config/settings.py
@@ -139,20 +139,24 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+import os
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'porunovsasha@gmail.com'      
-EMAIL_HOST_PASSWORD = 'hvcpjlhcygbzqbco'
-DEFAULT_FROM_EMAIL = 'porunovsasha@gmail.com>'    
+EMAIL_HOST_USER = 'apikey'      
+DEFAULT_FROM_EMAIL = 'porunovsasha@gmail.com' 
 
 LOGIN_REDIRECT_URL = '/projects/' 
 LOGOUT_REDIRECT_URL = '/'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-import os
+from dotenv import load_dotenv
+load_dotenv()
 
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
